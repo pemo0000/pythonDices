b6accd5a47b75f7fb1a9c62883426dcc69ca9ddf
pemo0000 <pemo0000@gmx.de>
2018-06-25 19:53:28 +0200

    +sound

--- dice.py
+++ dice.py
@@ -3,14 +3,13 @@ from random import randint
 
 class dice:
     def __init__(self):
-        return
+        pygame.mixer.init()
 
     def roll(self):
         self.result = randint(1, 6)
         return self.result
 
     def speak(self):
-        pygame.mixer.init()
         pygame.mixer.music.load(str(self.result) + 'se.wav')
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+#!/usr/bin/python3
 from dice import *
+import sys
+import readchar
 
 dice1 = dice()
 dice2 = dice()
-numberRolls = 0
+numberRolls = 2
 
 while True:
-    print(str(dice1.roll()) + ' ', end='')
-    dice1.speak()
-    print(dice2.roll())
-    dice2.speak()
-    input()
     numberRolls += 1
-    print(numberRolls)
+    if numberRolls & 1:
+        print(str(numberRolls >> 1) + '\t', end='')
+        print(str(dice1.roll()) + ' ', end='')
+        dice1.speak()
+        print(str(dice2.roll()) + ' ', end='')
+        dice2.speak()
+    else:
+        print ("\t", end='')
+        print(str(dice1.roll()) + ' ', end='')
+        dice1.speak()
+        print(str(dice2.roll()) + ' ', end='')
+        dice2.speak()
+        print('')
+
+    sys.stdout.flush()
+    readchar.readkey()
