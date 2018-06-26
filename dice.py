1cd6a6553e9b17fd0d6425c5c4a7f96eb4bc773a
pemo0000 <pemo0000@gmx.de>
2018-06-26 20:03:04 +0200

    sound refactored - pygame no longer needed

--- dice.py
+++ dice.py
@@ -1,17 +1,40 @@
-import pygame
+import ossaudiodev
+# import pygame
 from random import randint
+from wave import open as waveOpen
+from ossaudiodev import open as ossOpen
+from time import sleep
 
 class dice:
     def __init__(self):
-        pygame.mixer.init()
+        return
+        #pygame.mixer.init()
 
     def roll(self):
         self.result = randint(1, 6)
         return self.result
 
     def speak(self):
-        pygame.mixer.music.load(str(self.result) + 'se.wav')
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy() == True:
-            continue
+        #pygame.mixer.music.load(str(self.result) + 'se.wav')
+        #pygame.mixer.music.play()
+        #while pygame.mixer.music.get_busy() == True:
+        #    continue
+        s = waveOpen(str(self.result) + 'se.wav', 'rb')
+        (nc, sw, fr, nf, comptype, compname) = s.getparams()
+        dsp = ossOpen('/dev/dsp', 'w')
+        try:
+            from ossaudiodev import AFMT_S16_NE
+        except ImportError:
+            from sys import byteorder
+            if byteorder == "little":
+                AFMT_S16_NE = ossaudiodev.AFMT_S16_LE
+            else:
+                AFMT_S16_NE = ossaudiodev.AFMT_S16_BE
+        dsp.setparameters(AFMT_S16_NE, nc, fr)
+        data = s.readframes(nf)
+        s.close()
+        dsp.write(data)
+        # little sleep, otherwise .wavs are played too fast
+        sleep(0.35)
+        dsp.close()
 
--- main.py
+++ main.py
@@ -19,6 +19,8 @@ while True:
         dice1.speak()
         print(str(dice2.roll()) + ' ', end='')
         dice2.speak()
+        #if numberRolls == 3 and (dice1.result == dice2.result):
+        #    os.execv(__file__, sys.argv)
         connection = sqlite3.connect("/tmp/pythonDices.db")
         cursor = connection.cursor()
         sql = "INSERT INTO tosses VALUES(" + str(dice1.result) + "," + str(dice2.result) + ")"
